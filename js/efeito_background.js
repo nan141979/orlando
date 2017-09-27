9dba8019d4d4a66374a343f342343c821c16eb2f
Skynance <nan141979@hotmail.com>
2017-09-27 13:25:23 -0300

    primeiro commit

---
+++ js/efeito_background.js
@@ -0,0 +1,148 @@
+$(document).ready(function() {
+ $("#logotipo").on("mouseover", function(){
+
+        $("#banner h1").addClass("efeito");
+    }).on("mouseout", function(){
+        $("#banner h1").removeClass("efeito");
+    });
+
+/* ======== funcôes para animação de imagens ====  */
+    $(function(){
+            $("li.club-01 a").hover(
+                function(){ $("#banner").addClass("css_01") },
+                function(){ $("#banner").removeClass("css_01") }
+            );
+        }); //1
+    $(function(){
+            $("li.club-02 a").hover(
+                function(){ $("#banner").addClass("css_02") },
+                function(){ $("#banner").removeClass("css_02") }
+            );
+        }); //2
+    $(function(){
+            $("li.club-03 a").hover(
+                function(){ $("#banner").addClass("css_03") },
+                function(){ $("#banner").removeClass("css_03") }
+            );
+        }); //3
+    $(function(){
+        $("li.club-04 a").hover(
+            function(){ $("#banner").addClass("css_04") },
+            function(){ $("#banner").removeClass("css_04") }
+        );
+    }); //4
+    $(function(){
+        $("li.club-05 a").hover(
+            function(){ $("#banner").addClass("css_05") },
+            function(){ $("#banner").removeClass("css_05") }
+        );
+    }); //4
+    $(function(){
+        $("li.club-06 a").hover(
+            function(){ $("#banner").addClass("css_06") },
+            function(){ $("#banner").removeClass("css_06") }
+        );
+    }); //4
+    $(function(){
+        $("li.club-07 a").hover(
+            function(){ $("#banner").addClass("css_07") },
+            function(){ $("#banner").removeClass("css_07") }
+        );
+    }); //4
+    $(function(){
+        $("li.club-08 a").hover(
+            function(){ $("#banner").addClass("css_08") },
+            function(){ $("#banner").removeClass("css_08") }
+        );
+    }); //4
+    $(function(){
+        $("li.club-09 a").hover(
+            function(){ $("#banner").addClass("css_09") },
+            function(){ $("#banner").removeClass("css_09") }
+        );
+    }); //4
+    $(function(){
+        $("li.club-10 a").hover(
+            function(){ $("#banner").addClass("css_10") },
+            function(){ $("#banner").removeClass("css_10") }
+        );
+    }); //4
+    $(function(){
+        $("li.club-11 a").hover(
+            function(){ $("#banner").addClass("css_11") },
+            function(){ $("#banner").removeClass("css_11") }
+        );
+    }); //4
+    $(function(){
+        $("li.club-12 a").hover(
+            function(){ $("#banner").addClass("css_12") },
+            function(){ $("#banner").removeClass("css_12") }
+        );
+    }); //4
+    $(function(){
+        $("li.club-13 a").hover(
+            function(){ $("#banner").addClass("css_13") },
+            function(){ $("#banner").removeClass("css_13") }
+        );
+    }); //4
+    $(function(){
+        $("li.club-14 a").hover(
+            function(){ $("#banner").addClass("css_14") },
+            function(){ $("#banner").removeClass("css_14") }
+        );
+    }); //4
+    $(function(){
+        $("li.club-15 a").hover(
+            function(){ $("#banner").addClass("css_15") },
+            function(){ $("#banner").removeClass("css_15") }
+        );
+    }); //4
+    $(function(){
+        $("li.club-16 a").hover(
+            function(){ $("#banner").addClass("css_16") },
+            function(){ $("#banner").removeClass("css_16") }
+        );
+    }); //4
+    $(function(){
+        $("li.club-17 a").hover(
+            function(){ $("#banner").addClass("css_17") },
+            function(){ $("#banner").removeClass("css_17") }
+        );
+    }); //4
+    $(function(){
+        $("li.club-18 a").hover(
+            function(){ $("#banner").addClass("css_18") },
+            function(){ $("#banner").removeClass("css_18") }
+        );
+    }); //4
+    $(function(){
+        $("li.club-19 a").hover(
+            function(){ $("#banner").addClass("css_19") },
+            function(){ $("#banner").removeClass("css_19") }
+        );
+    }); //4
+    $(function(){
+        $("li.club-20 a").hover(
+            function(){ $("#banner").addClass("css_20") },
+            function(){ $("#banner").removeClass("css_20") }
+        );
+    }); //4
+    $(function(){
+        $("li.club-21 a").hover(
+            function(){ $("#banner").addClass("css_21") },
+            function(){ $("#banner").removeClass("css_21") }
+        );
+    }); //4
+    $(function(){
+        $("li.club-22 a").hover(
+            function(){ $("#banner").addClass("css_22") },
+            function(){ $("#banner").removeClass("css_22") }
+        );
+    }); //4
+
+
+
+
+
+
+});// fim document.ready
